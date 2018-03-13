9352ee86de9244e5df4041ce51947700a79709a1
Spomni <mikhail.snitko@gmail.com>
2018-03-13 18:05:39 +0300

    #5.1 Check input params

---
+++ dev/lottotron.js
@@ -0,0 +1,45 @@
+/** @function Lottotron
+  * @constructor
+  *
+  * @classdesc Создает класс, способный выдавать натуральные числа в произвольном порядке из заданного диапазона, без их повторения.
+  *
+  * @param {number} maxNumber - Верхняя граница диапазона. Число должно быть больше либо равно нулю. Дробные значения будут округлены до целого в меньшую сторону.
+  *
+  * @returns {Lottotron}
+  */
+  function Lottotron( maxNumber ){
+  
+    //-- Check input param
+      if ( typeof(maxNumber) != 'number' ){
+        return new Error('The input option "maxNumber" should be a number.');
+      } else if ( maxNumber < 0 ){
+        return new Error('The input option "maxNumber" should be greater than 0.');
+      }
+  
+    /** @property {number} maxNumber - Верхняя граница диапазона.
+      * @public
+      * @protected
+      */
+  
+    /** @property {array} restNumbers - Массив чисел диапазона, которые не были возвращены методом getNumber.
+      * @public
+      * @protected
+      */
+    
+    /** @method getNumber Возвращает следующее случайное число диапазона.
+      * @public
+      *
+      * @desc Возвращает следующее случайное число диапазона. Возвращает null, если все числа диапазона были выданы ранее.
+      * 
+      * @returns {number|null}
+      */
+      
+    /** @method reload Очищает память выдачи чисел
+      * @public
+      *
+      * @desc Очищает память выдачи. Посли вызова данного метода, метод getNumber не учитывает числа, выданные до текущего момента.
+      *
+      * @returns {undefined}
+      */
+    
+  }
--- test/spec/lottotron-spec.js
+++ test/spec/lottotron-spec.js
@@ -1,4 +1,58 @@
-describe('Test a test environment', function(){
-  it( 'Test a test environment', function(){
+describe( 'lottotron.js', function(){
+
+  /** @namespace itWrappers */
+  var itWr, itWrappers;
+  itWr = itWrappers = {
+  
+    /** @function forEachType Вызывает функцию callback для каждого из стандартных типов данных.
+      * @describe Вызывает функцию callback для каждого из стандартных типов данных. В параметр type передает экщемпляр соответствующего типа данеых.
+      * @param {Function} callback( type )
+      */
+    forEachType : function( callback ){
+    
+      var typeArr = [
+        1,
+        "string",
+        null,
+        undefined,
+        [],
+        {}
+      ]
+      
+      var res = function(){
+        typeArr.forEach(function(type, i, typeArr){
+          callback( type );
+        });
+      }
+      
+      return res;
+    }
+    
+  } // itWrapper
+  
+  function isNumber( param ){
+    return ( typeof( param ) == "number" );
+  }
+
+  describe('Check the constructor "Lottotron"', function(){
+  
+    it( 'Should return an "Error" object if the input param "maxNumber" is not number.', itWr.forEachType(function( type ){
+      if ( !isNumber( type ) ){
+        var lotto = new Lottotron( type );
+        assert.instanceOf( lotto, Error );
+      }
+    }));
+    
+    it( 'Should return an "Error" object if the input param "maxNumber" is less than 0.', function(){
+      var lotto = new Lottotron( -3 );
+      assert.instanceOf( lotto, Error );
+    });
+    
+    it( 'Should return an object instance of "Lottotron" if the input param "maxNumber" is number and it is not less than 0.', function(){
+      var lotto = new Lottotron( 8 );
+      assert.instanceOf( lotto, Lottotron );
+    });
+    
   });
+
 });
